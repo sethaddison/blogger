609fc14f6af3734eb88267a1ab2d657be71d9103
Seth Cox <sethaddison@gmail.com>
2020-10-09 15:47:31 -0700

    Add full article resource

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,49 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.order(created_at: :desc)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params)
+    if @article.save
+      redirect_to @article, notice: "Your article was created!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article, notice: "Your article was updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
+end
