c8760c5039c5f5c313c738a5be2cde76eec3dbe8
Seth Cox <sethaddison@gmail.com>
2020-10-10 15:30:36 -0700

    Build tagging feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,21 @@
 class Article < ApplicationRecord
+
   has_many :comments, dependent: :destroy
+  has_many :taggings
+  has_many :tags, through: :taggings
+
   validates :title, :body, presence: true
+
+  def tag_list
+    self.tags.map do |t|
+      t.name
+    end.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").map { |s| s.strip.downcase }.uniq
+    new_or_found_tags = tag_names.map { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
+
 end
